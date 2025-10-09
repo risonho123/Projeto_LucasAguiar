b5c4f88c8331ce8a245a1cb0e3d75334bf98d72b
arthur0321 <160272054+arthur0321@users.noreply.github.com>
2025-10-09 17:25:35 -0400

    real oficial commit

---
+++ lucasaguiar.arquivofinal/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace BarbeariaLucasAaguiar.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Cpf { get; set; }
+        public DateTime DataAniversario { get; set; }
+        public string? Rg { get; set; }
+    }
+}
