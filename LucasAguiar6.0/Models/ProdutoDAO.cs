b099f555b8438f048ca302892a9005e6cf751a1f
orlando <159702896+risonho123@users.noreply.github.com>
2025-11-24 10:29:36 -0300

    Produto(excluir /editar)

--- LucasAguiar6.0/Models/ProdutoDAO.cs
+++ LucasAguiar6.0/Models/ProdutoDAO.cs
@@ -1,4 +1,6 @@
 using LucasAguiar.Configs;
+using MySqlConnector;
+using AppWeb.Configs;
 
 namespace LucasAguiar.Models
 {
@@ -34,6 +36,71 @@ namespace LucasAguiar.Models
                 throw new Exception("Erro ao inserir produto: " + ex.Message);
             }
         }
+         public Produto? BuscarPorId(int id)
+        {
+            var comando = _conexao.CreateCommand(
+                "SELECT * FROM produto WHERE id_prod = @id;");
+            comando.Parameters.AddWithValue("@id", id);
+
+            var leitor = comando.ExecuteReader();
+
+            if (leitor.Read())
+            {
+                var produto = new Produto();
+                produto.IdProduto = leitor.GetInt32("id_prod");
+                produto.NomeProduto = DAOHelper.GetString(leitor, "nome_prod");
+                produto.Descricao = DAOHelper.GetString(leitor, "descricao_prod");
+                produto.Marca = DAOHelper.GetString(leitor, "marca_prod");
+                produto.Quantidade = leitor.GetInt32("quantidade_prod");
+                produto.Valor = leitor.GetFloat("valor_prod");
+
+                return produto;
+            }
+            else
+            {
+                return null;
+            }
+        }
+public void Atualizar(Produto produto)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                "UPDATE produto SET nome_prod = @_nome, descricao_prod = @_descricao, marca_prod = @_marca," +
+                "quantidade_prod = @_quantidade, valor_prod = @_preco WHERE id_prod = @_id;");
+
+                comando.Parameters.AddWithValue("@_nome", produto.NomeProduto);
+                comando.Parameters.AddWithValue("@_descricao", produto.Descricao);
+                comando.Parameters.AddWithValue("@_marca", produto.Marca);
+                comando.Parameters.AddWithValue("@_quantidade", produto.Quantidade);
+                comando.Parameters.AddWithValue("@_preco", produto.Valor);
+                comando.Parameters.AddWithValue("@_id", produto.IdProduto);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                "DELETE FROM produto WHERE id_prod = @id;");
+
+                comando.Parameters.AddWithValue("@id", id);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
 
         public List<Produto> ListarTodos()
         {
@@ -44,26 +111,46 @@ namespace LucasAguiar.Models
                 INNER JOIN fornecedor f ON p.id_forn_fk = f.id_forn
             ");
 
-            var leitor = comando.ExecuteReader();
+           var leitor = comando.ExecuteReader();
 
-            while (leitor.Read())
-            {
-                var produto = new Produto
-                {
-                    IdProduto = leitor.GetInt32("id_prod"),
-                    NomeProduto = leitor.GetString("nome_prod"),
-                    Quantidade = leitor.GetInt32("quantidade_prod"),
-                    Valor = leitor.GetFloat("valor_prod"),
-                    Descricao = leitor.GetString("descricao_prod"),
-                    Marca = leitor.GetString("marca_prod"),
-                    IdFornecedor = leitor.GetInt32("id_forn_fk"),
-                    NomeFornecedor = leitor.GetString("nome_forn")
-                };
+        while (leitor.Read())
+    {
+    var produto = new Produto
+    {
+        IdProduto = leitor.IsDBNull(leitor.GetOrdinal("id_prod")) 
+            ? 0 : leitor.GetInt32("id_prod"),
 
-                lista.Add(produto);
-            }
+        NomeProduto = leitor.IsDBNull(leitor.GetOrdinal("nome_prod")) 
+            ? "" : leitor.GetString("nome_prod"),
 
-            return lista;
-        }
+        Quantidade = leitor.IsDBNull(leitor.GetOrdinal("quantidade_prod")) 
+            ? 0 : leitor.GetInt32("quantidade_prod"),
+
+        Valor = leitor.IsDBNull(leitor.GetOrdinal("valor_prod")) 
+            ? 0 : leitor.GetFloat("valor_prod"),
+
+        Descricao = leitor.IsDBNull(leitor.GetOrdinal("descricao_prod")) 
+            ? "" : leitor.GetString("descricao_prod"),
+
+        Marca = leitor.IsDBNull(leitor.GetOrdinal("marca_prod")) 
+            ? "" : leitor.GetString("marca_prod"),
+
+        IdFornecedor = leitor.IsDBNull(leitor.GetOrdinal("id_forn_fk")) 
+            ? 0 : leitor.GetInt32("id_forn_fk"),
+
+        NomeFornecedor = leitor.IsDBNull(leitor.GetOrdinal("nome_forn")) 
+            ? "" : leitor.GetString("nome_forn")
+    };
+
+    lista.Add(produto);
+
+
+
+    
     }
+
+    return lista;
+
+        }
+        }
 }
